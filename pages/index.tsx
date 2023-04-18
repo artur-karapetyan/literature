1162ad7f1e953007353b55b4c177aff278bcef77
Artur Karapetyan <artur.karapetyan.2002@gmail.com>
2023-04-18 11:02:18 +0400

    UPD:

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import Head from "next/head";
 import { useState, useEffect } from "react";
 
 interface Book {
+  id: number;
   title: string;
   author: string;
+  image: string;
   description: string;
   rating: number;
 }
@@ -31,18 +33,17 @@ export default function Books() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="flex flex-row justify-start items-center w-full h-full gap-10 px-[150px] pt-[100px] pb-[50px] bg-[#f6f5f5]">
-        <div className="flex flex-wrap justify-start items-start flex-grow-0 flex-shrink-0 w-full h-full overflow-hidden pt-[30px] pb-[40px] px-[20px] gap-2.5">
-          {books.map((book) => (
-            <div
-              className="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 w-[285px] h-[390px] overflow-hidden gap-2.5 p-2.5 border border-[#f4c785] cursor-pointer shadow-lg hover:scale-110 hover:bg-[#fef6eb] dark:hover:bg-gray-700 dark:bg-gray-900 dark:shadow-orange-600 duration-500"
-              key={book.title}
-            >
-              <p>{book.title}</p>
-              <p>{book.author}</p>
-            </div>
-          ))}
-        </div>
+      <div className="flex flex-wrap justify-center items-center w-full min-h-screen gap-2.5 p-10 bg-[#f6f5f5]">
+        {books.map((book) => (
+          <div
+            className="book-card flex flex-col justify-start items-center w-full max-w-xs p-4 my-4 bg-white rounded-md shadow-lg hover:scale-110 hover:bg-[#fef6eb] dark:hover:bg-gray-700 dark:bg-gray-900 dark:shadow-orange-600 duration-500"
+            key={book.id}
+          >
+            <img src={book.image} alt={book.title} />
+            <p className="text-lg font-bold">{book.title}</p>
+            <p className="text-sm font-medium text-gray-500">{book.author}</p>
+          </div>
+        ))}
       </div>
       <footer className="bg-gray-800 py-8">
         <div className="container mx-auto px-4">
@@ -52,6 +53,11 @@ export default function Books() {
           </p>
         </div>
       </footer>
+      <style jsx>{`
+        .book-card {
+          height: 400px; /* Set the height to 400px */
+        }
+      `}</style>
     </>
   );
 }
